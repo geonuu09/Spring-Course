bdd495f5fe34dc0ffc1d22a3f6af60e93efeab13
LeeGeonu <rjsdndl909@gmail.com>
2024-11-05 14:56:45 +0900

    validation

--- src/main/java/com/sparta/springauth/controller/ProductController.java
+++ src/main/java/com/sparta/springauth/controller/ProductController.java
@@ -1,11 +1,19 @@
 package com.sparta.springauth.controller;
 
+import com.sparta.springauth.dto.ProductRequestDto;
 import com.sparta.springauth.entity.User;
+import com.sparta.springauth.entity.UserRoleEnum;
 import com.sparta.springauth.security.UserDetailsImpl;
+import jakarta.validation.Valid;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/api")
@@ -19,5 +27,22 @@ public class ProductController {
     System.out.println("user.getEmail() = " + user.getEmail());
 
     return "redirect:/";
+  }
+
+  @Secured(UserRoleEnum.Authority.ADMIN) // 관리자용
+  @GetMapping("/products/secured")
+  public String getProductsByAdmin(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+    System.out.println("userDetails.getUsername() = " + userDetails.getUsername());
+    for (GrantedAuthority authority : userDetails.getAuthorities()) {
+      System.out.println("authority.getAuthority() = " + authority.getAuthority());
+    }
+
+    return "redirect:/";
+  }
+
+  @PostMapping("/validation")
+  @ResponseBody
+  public ProductRequestDto testValid(@RequestBody @Valid ProductRequestDto requestDto) {
+    return requestDto;
   }
 }
